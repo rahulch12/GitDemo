72750182f139b8f77592a984e9c107b9f840f6e0
rahul <chauhanr003@gmail.com>
2021-09-30 10:12:38 +0530

    American guy commit

--- Mavenjava/src/test/java/qaclickacademy/Mavenjava/RestAPITest.java
+++ Mavenjava/src/test/java/qaclickacademy/Mavenjava/RestAPITest.java
@@ -10,6 +10,9 @@ public class RestAPITest {
 		System.out.println("postJira");
 		System.out.println("postJira2");
 		System.out.println("postJira3");
+		System.out.println("postJira4");
+		System.out.println("postJira5");
+		System.out.println("postJira6");
 	}
 	@Test
 	public void deleteTwitter()
